e17ef39719e0dca29e0f1c728c1d45b0ab7e2293
Al-Imran <alimran21feb@gmail.com>
2022-04-13 20:04:36 +0600

    checked env file

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const nameRef = useRef ('');
+    const emailRef = useRef ('');
+    const passwordRef = useRef ();
 
-    const navigate = useNavigate ();
+    const navigate = useNavigate();
 
     const navigateLogin = (event) => {
-        navigate ('/login');
+        navigate('/login');
 
-    } 
+    }
+
 
     const handleRegister = (event) => {
         event.preventDefault ();
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log (name, email, password);
+
     }
 
     return (
@@ -24,12 +30,12 @@ const Register = () => {
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Your Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" required />
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter Name" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -37,7 +43,7 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
